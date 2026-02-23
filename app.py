09b3fbc3ff7341266237a8a03b6ed073ec6c8936
Ankit Mistry <ankit.25587@gmail.com>
2026-02-23 12:31:06 +0530

    added basic code

---
+++ app.py
@@ -0,0 +1,7 @@
+# add some simple python code here to demonstrate the workflow
+def main():
+    print("Hello, World!")
+    print("This is a simple workflow demo.")
+
+if __name__ == "__main__":    
+    main()
